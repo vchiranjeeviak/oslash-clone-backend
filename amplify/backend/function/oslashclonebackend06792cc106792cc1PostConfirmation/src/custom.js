aab0dd5fcdec285bc98b2941d7ca93c54f7dffe7
Chiranjeevi Tirunagari <vchiranjeeviak.tirunagari@gmail.com>
2022-06-11 02:33:15 +0530

    Wrote a post-confirmation lambda trigger function to add users to db on signup

--- amplify/backend/function/oslashclonebackend06792cc106792cc1PostConfirmation/src/custom.js
+++ amplify/backend/function/oslashclonebackend06792cc106792cc1PostConfirmation/src/custom.js
@@ -1,7 +1,38 @@
-/**
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
- */
+var aws = require("aws-sdk");
+var ddb = new aws.DynamoDB();
+
 exports.handler = async (event, context) => {
-  // insert code to be executed by your lambda trigger
-  return event
+  let date = new Date(); // Creating a new data object
+
+  if (event.request.userAttributes.sub) {
+    // If we can find ID in the event request and ID is labelled as sub, only then we proceed to add user to user table in DB
+
+    let params = {
+      // Creating the object to insert
+      Item: {
+        id: { S: event.request.userAttributes.sub },
+        __typename: { S: "User" },
+        username: { S: event.userName },
+        email: { S: event.request.userAttributes.email },
+        createdAt: { S: date.toISOString() },
+      },
+      TableName: process.env.USERTABLE, // Table name is set to users table as an environment variable in AWS console
+    };
+
+    try {
+      // Try putting item into DB
+
+      await ddb.putItem(params).promise();
+      console.log("Success");
+    } catch (error) {
+      // If got error while putting item into DB
+
+      console.log("Error", error);
+    }
+
+    context.done(null, event);
+  } else {
+    console.log("Error");
+    context.done(null, event);
+  }
 };
